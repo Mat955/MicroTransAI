bf70ab9f8e86937913047394a8a33b8d01ee3c3e
Mateusz Lewartowski <lewar2013c@gmail.com>
2024-04-04 14:32:32 +0200

    Add Header component and update layout

---
+++ app/components/Header.tsx
@@ -0,0 +1,34 @@
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../translate-logo.png";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+function Header() {
+  const { userId } = auth();
+  return (
+    <header className="flex items-center justify-between px-8 border-b mb-5">
+      <div className="flex items-center justify-center h-20">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="logo"
+            width={200}
+            height={100}
+            className="object-contain h-32 cursor-pointer"
+          />
+        </Link>
+      </div>
+
+      {userId ? (
+        <div>
+          <UserButton />
+        </div>
+      ) : (
+        <SignInButton afterSignInUrl="/translate" mode="modal" />
+      )}
+    </header>
+  );
+}
+
+export default Header;
--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -3,7 +3,10 @@ import { auth } from "@clerk/nextjs/server";
 function TranslatePage() {
   auth().protect();
 
-  return <div>TranslatePage</div>;
+  const { userId } = auth();
+  if (!userId) throw new Error("User not logged in");
+
+  return <div></div>;
 }
 
 export default TranslatePage;
